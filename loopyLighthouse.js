9e2c6ea4ba7b2f5d5f95168131b187c9d5adb8c2
cniscoding <asdfa.com>
2022-12-13 12:47:12 -0800

    loopy

---
+++ loopyLighthouse.js
@@ -0,0 +1,7 @@
+let i = 0;
+for (i = 100; i <= 200; i++){
+  i % 3 === 0 && i % 4 === 0 ? console.log("LoopyLighthouse")
+    :i % 3 === 0 ? console.log("Loopy")
+    :i % 4 === 0 ? console.log("Lighthouse")
+    :console.log(i);
+}
